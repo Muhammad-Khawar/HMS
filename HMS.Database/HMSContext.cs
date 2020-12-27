0414ce3ca1ce617a94e68efc94b201ffb9ac7e2e
Khawar <khawargsabir@gmail.com>
2020-12-28 02:01:14 +0500

    First Lecture Done

---
+++ HMS.Database/HMSContext.cs
@@ -0,0 +1,28 @@
+﻿using HMS.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Database
+{
+    public class HMSContext : IdentityDbContext<HMSUser>
+    {
+        public HMSContext() : base("HMSConnectionString")
+        {
+        }
+        public static HMSContext Create()
+        {
+            return new HMSContext();
+        }
+
+        public DbSet<AccommodationType> AccommodationType { get; set; }
+        public DbSet<AccommodationPackage> AccommodationPackage { get; set; }
+        public DbSet<Accommodation> Accommodation { get; set; }
+        public DbSet<Booking> Booking { get; set; }
+
+    }
+}
